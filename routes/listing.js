cc87aa20322fb8fe2bd6a9d838499a526f24ef36
preshu100 <preeeti530@gmail.com>
2024-05-01 12:40:00 +0530

    Add new files

--- routes/listing.js
+++ routes/listing.js
@@ -26,6 +26,7 @@ router.route("/:id")
 .get(wrapAsync(listingController.showListing))
 
 .put(isLoggedIn, isOwner,
+   upload.single("listing[image]"),
 validateListing,
 wrapAsync(listingController.updateListing))
 
